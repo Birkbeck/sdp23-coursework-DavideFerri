a786bc07132b8c5462a38bfd8091338accf80d63
DAVIDE <davide.ferri.94@gmail.com>
2023-02-19 23:09:34 +0000

    Added javadoc to Instruction and AddInstruction

--- src/sml/Instruction.java
+++ src/sml/Instruction.java
@@ -2,56 +2,109 @@ package sml;
 
 // TODO: write a JavaDoc for the class
 
+import java.util.Objects;
+
 /**
- * Represents an abstract instruction.
+ * The Instruction class represents an abstract instruction in a machine language program.
  *
- * @author ...
+ * This class provides functionality for retrieving the label and opcode of an instruction, as well as a method for
+ * executing an instruction on a Machine object. Subclasses of this class should implement the execute, toString,
+ * equals, and hashCode methods to provide their own behavior.
  */
 public abstract class Instruction {
+
+	/** The optional label for the instruction. */
 	protected final String label;
+
+	/** The opcode for the instruction. */
 	protected final String opcode;
 
+	/** A constant value indicating how to update the program counter. */
+	public static int NORMAL_PROGRAM_COUNTER_UPDATE = -1;
+
 	/**
-	 * Constructor: an instruction with a label and an opcode
-	 * (opcode must be an operation of the language)
+	 * Constructs an Instruction object with a label and opcode.
 	 *
-	 * @param label optional label (can be null)
-	 * @param opcode operation name
+	 * @param label the optional label for the instruction, or null if none
+	 * @param opcode the opcode for the instruction
 	 */
 	public Instruction(String label, String opcode) {
 		this.label = label;
 		this.opcode = opcode;
 	}
 
+	/**
+	 * Returns the label for the instruction.
+	 *
+	 * @return the label for the instruction, or null if none
+	 */
 	public String getLabel() {
 		return label;
 	}
 
+	/**
+	 * Returns the opcode for the instruction.
+	 *
+	 * @return the opcode for the instruction
+	 */
 	public String getOpcode() {
 		return opcode;
 	}
 
-	public static int NORMAL_PROGRAM_COUNTER_UPDATE = -1;
-
 	/**
-	 * Executes the instruction in the given machine.
+	 * Executes the instruction on the specified Machine object.
 	 *
-	 * @param machine the machine the instruction runs on
-	 * @return the new program counter (for jump instructions)
-	 *          or NORMAL_PROGRAM_COUNTER_UPDATE to indicate that
-	 *          the instruction with the next address is to be executed
+	 * This method should be implemented by subclasses of Instruction to provide their own behavior. The method returns
+	 * an integer value
+	 *
+	 * @param machine the Machine object on which to execute the instruction
+	 * @return an integer
 	 */
-
 	public abstract int execute(Machine machine);
 
+
+	/**
+	 * Returns the label string for this instruction, or the empty string if no label is present.
+	 *
+	 * This method is used to construct the string representation of the instruction. If a label is present, it is
+	 * included in the string with a trailing colon; otherwise, the string is empty.
+	 *
+	 * @return the label string for this instruction, or the empty string if no label is present
+	 */
 	protected String getLabelString() {
 		return (getLabel() == null) ? "" : getLabel() + ": ";
 	}
 
-	// TODO: What does abstract in the declaration below mean?
-	//       (Write a short explanation.)
+	/**
+	 * Returns a string representation of the instruction.
+	 *
+	 * This method should be implemented by subclasses of Instruction to provide their own behavior. The string should
+	 * be formatted as "label: opcode args" if a label is present, or "opcode args" otherwise.
+	 *
+	 * @return a string representation of the instruction
+	 */
 	@Override
 	public abstract String toString();
 
-	// TODO: Make sure that subclasses also implement equals and hashCode (needed in class Machine).
+	/**
+	 * Compares this Instruction to the specified object for equality.
+	 *
+	 * This method should be implemented by subclasses of Instruction to provide their own behavior.
+	 *
+	 * @param o the object to compare to this Instruction
+	 * @return true if the specified object is equal to this Instruction, false otherwise
+	 */
+	@Override
+	public abstract boolean equals(Object o);
+
+	/**
+	 * Returns a hash code value for this Instruction.
+	 *
+	 * This method should be implemented by subclasses of Instruction to provide their own behavior.
+	 *
+	 * @return a hash code value for this Instruction
+	 */
+	@Override
+	public abstract int hashCode();
 }
+
--- src/sml/instruction/AddInstruction.java
+++ src/sml/instruction/AddInstruction.java
@@ -4,24 +4,44 @@ import sml.Instruction;
 import sml.Machine;
 import sml.RegisterName;
 
-// TODO: write a JavaDoc for the class
-
 /**
- * @author
+ * The AddInstruction class represents an instruction in a machine language program that adds the value of one register
+ * to another register and stores the result in the first register. This instruction extends the Instruction class.
  */
-
 public class AddInstruction extends Instruction {
+
+	/** The name of the result register. */
 	private final RegisterName result;
+
+	/** The name of the source register. */
 	private final RegisterName source;
 
+	/** The opcode for the add instruction. */
 	public static final String OP_CODE = "add";
 
+	/**
+	 * Constructs an AddInstruction object with a label, result register name, and source register name.
+	 *
+	 * @param label the label for the instruction, or null if none
+	 * @param result the name of the result register
+	 * @param source the name of the source register
+	 */
 	public AddInstruction(String label, RegisterName result, RegisterName source) {
 		super(label, OP_CODE);
 		this.result = result;
 		this.source = source;
 	}
 
+	/**
+	 * Executes the add instruction on the specified Machine object.
+	 *
+	 * This method retrieves the values of the two registers specified in the constructor, adds them together, and stores
+	 * the result in the result register. The method returns a constant value indicating that the program counter should be
+	 * updated normally.
+	 *
+	 * @param m the Machine object on which to execute the instruction
+	 * @return the value to add to the program counter after the instruction is executed
+	 */
 	@Override
 	public int execute(Machine m) {
 		int value1 = m.getRegisters().get(result);
@@ -30,8 +50,14 @@ public class AddInstruction extends Instruction {
 		return NORMAL_PROGRAM_COUNTER_UPDATE;
 	}
 
+	/**
+	 * Returns a string representation of the instruction in the format "label: opcode result source".
+	 *
+	 * @return a string representation of the instruction
+	 */
 	@Override
 	public String toString() {
 		return getLabelString() + getOpcode() + " " + result + " " + source;
 	}
 }
+
